929add871f1a7b34cca3dde8bedafd6c3c6728b9
SaqibAltaf <saqib.a@urent.com>
2022-04-03 14:21:37 +0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+
+#### Follow these command lines:
+1. Opne terminal 
+2. run `npm install`
+3. run `npm start`
+
+
+Happy Coding :)
+
---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+export default class config {
+    // place for some confidential things like api key services key etc.
+
+    static mongodbUrl() {
+        return "mongodb://localhost:27017/db"
+    }
+
+};
